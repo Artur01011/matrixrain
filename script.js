593d8547fb20ba71ee313c384710b23198ddd94a
Artur <9362561@gmail.com>
2023-07-02 11:46:28 -0800

    The symbol rain effect achived.

--- script.js
+++ script.js
@@ -4,11 +4,24 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 class Symbol {
-    constructor(){
-
+    constructor(x, y, fontSize, canvasHeight){
+        this.characters = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+        this.x = x;
+        this.y = y;
+        this.fontSize = fontSize;
+        this.text = '';
+        this.canvasHeight = canvasHeight;
     }
-    draw(){
 
+    draw(context){
+        this.text = this.characters.charAt(Math.floor(Math.random() * this.characters.length));
+        context.fillStyle = 'green';
+        context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
+        if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.9){
+            this.y = 0;
+        } else {
+            this.y += 1;
+        }
     }
 }
 
@@ -19,11 +32,25 @@ class Effect {
         this.fontSize = 25;
         this.columns = this.canvasWidth / this.fontSize;
         this.symbols = [];
+        this.#initialaze();
     }
+
     #initialaze(){
-
+        for (let i = 0; i < this.columns; i++) {
+            this.symbols[i] = new Symbol(i, 0, this.fontSize, this.canvasHeight);
+            
+        }
     }
 }
-function animate(){
 
+const effect = new Effect(canvas.width, canvas.height);
+
+function animate(){
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.font = effect.fontSize + 'px monospace';
+    effect.symbols.forEach(symbol => symbol.draw(ctx));
+    requestAnimationFrame(animate);
 }
+
+animate();
