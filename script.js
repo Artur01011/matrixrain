77546f24f20d2defbf3a36fabf0fc6fc5cab4a81
Artur <9362561@gmail.com>
2023-07-04 10:24:37 -0800

    Added chinese letters, speed and resize.

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 class Symbol {
-    constructor(x, y, fontSize, canvasHeight){
-        this.characters = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    constructor(x, y, fontSize, canvasHeight) {
+        this.characters = '是石好你杰米响凡同不あいうえおかきくけこさしすせそ1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
         this.x = x;
         this.y = y;
         this.fontSize = fontSize;
@@ -13,11 +13,10 @@ class Symbol {
         this.canvasHeight = canvasHeight;
     }
 
-    draw(context){
+    draw(context) {
         this.text = this.characters.charAt(Math.floor(Math.random() * this.characters.length));
-        context.fillStyle = 'green';
         context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
-        if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.9){
+        if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.98) {
             this.y = 0;
         } else {
             this.y += 1;
@@ -26,7 +25,7 @@ class Symbol {
 }
 
 class Effect {
-    constructor(canvasWidth, canvasHeight){
+    constructor(canvasWidth, canvasHeight) {
         this.canvasWidth = canvasWidth;
         this.canvasHeight = canvasHeight;
         this.fontSize = 25;
@@ -35,22 +34,49 @@ class Effect {
         this.#initialaze();
     }
 
-    #initialaze(){
+    #initialaze() {
         for (let i = 0; i < this.columns; i++) {
             this.symbols[i] = new Symbol(i, 0, this.fontSize, this.canvasHeight);
-            
+
         }
     }
 }
-
-const effect = new Effect(canvas.width, canvas.height);
-
-function animate(){
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.font = effect.fontSize + 'px monospace';
-    effect.symbols.forEach(symbol => symbol.draw(ctx));
-    requestAnimationFrame(animate);
+resize(width, height){
+    this.canvasWidth = width;
+    this.canvasHeight = height;
+    this.columns = this.canvasWidth / this.fontSize;
+    this.symbols = [];
+    this.#initialize;
 }
 
-animate();
+
+const effect = new Effect(canvas.width, canvas.height);
+let lastTime = 0;
+const fps = 50;
+const nextFrame = 1000 / fps;
+let timer = 0;
+
+function animate(timeStamp) {
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+    if (timer > nextFrame) {
+        ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
+        ctx.textAlign = 'center';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = 'green';
+        ctx.font = effect.fontSize + 'px monospace';
+        effect.symbols.forEach(symbol => symbol.draw(ctx));
+        timer = 0;
+    } else {
+        timer += deltaTime;
+    }
+
+    requestAnimationFrame(animate);
+}
+animate(0);
+
+window.addEventListener('resize', function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    effect.resize(canvas.width, canvas.height);
+});
